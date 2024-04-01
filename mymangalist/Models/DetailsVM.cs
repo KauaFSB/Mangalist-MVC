7fb297b040358d66d2c3facb9b759da6fb2453c5
KauaFSB <kauaflp234@gmail.com>
2024-04-01 19:45:48 -0300

    foram eeditados e corrigidos alguns erros

---
+++ mymangalist/Models/DetailsVM.cs
@@ -0,0 +1,12 @@
+namespace mymangalist.Models;
+
+public class DetailsVM
+{
+    public Manga Anterior {get; set; }
+
+    public Manga  Atual { get; set; }
+
+    public Manga Proximo { get; set;}
+
+    public List<Genero> Tipos { get; set; }
+}
